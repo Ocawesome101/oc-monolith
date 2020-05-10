dd8faf844abecc36971368b44698892446e93c79
Ocawesome101 <ocawesome101@gmail.com>
2020-05-10 19:33:16 -0400

    aaaaaaaaaaaaaaaa

--- build/bin/mv.lua
+++
@@ -1,8 +0,0 @@
--- mv --
-
-local shell = require("shell")
-
-local args, opts = shell.parse(...)
-
-shell.execute("cp -r", table.unpack(args))
-shell.execute("rm -r", table.unpack(args, 1, #args - 1))
--- build/bin/uptime.lua
+++
@@ -1,3 +0,0 @@
--- uptime --
-
-print(require("time").formatTime(require("computer").uptime(), "s", true))
