a32d6a95198635e5d3f150ebb80faea4bda3d3c0
Ocawesome101 <ocawesome101@gmail.com>
2020-05-12 16:23:24 -0400

    add release CPIO

--- kernel/kernel.lua
+++ kernel/kernel.lua
@@ -7,7 +7,7 @@ flags.init = flags.init or "/sbin/init.lua"
 flags.quiet = flags.quiet or false
 
 local _KERNEL_NAME = "Monolith"
-local _KERNEL_REVISION = "2a882a5"
+local _KERNEL_REVISION = "5508a56"
 local _KERNEL_BUILDER = "ocawesome101@manjaro-pbp"
 local _KERNEL_COMPILER = "luacomp 1.2.0"
 
