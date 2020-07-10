92caf2a08fff08660495c7dcd10911f3f1c08a10
Ocawesome101 <ocawesome101@gmail.com>
2020-07-10 00:07:40 -0400

    re-license under GPLv3
    yes, another license change

--- kernel/base.lua
+++ kernel/base.lua
@@ -1,18 +1,19 @@
---[[ The core.
+--[[    
+        The core of the Monolith kernel.
+        Copyright (C) 2020 Ocawesome101
 
-Copyright 2020 Ocawesome101
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
 
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. ]]--
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>. ]]
 
 _G._START = computer.uptime()
 
@@ -21,7 +22,7 @@ flags.init = flags.init or "/sbin/init.lua"
 flags.quiet = flags.quiet or false
 
 local _KERNEL_NAME = "Monolith"
-local _KERNEL_REVISION = "2020.7.9"
+local _KERNEL_REVISION = "2020.7.10"
 
 _G._OSVERSION = string.format("%s version %s", _KERNEL_NAME, _KERNEL_REVISION)
 
