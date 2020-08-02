90548a08623360e15070d7704b08e14dfbb17792
Ocawesome101 <ocawesome101@gmail.com>
2020-08-01 20:57:00 -0400

    remove "git client"

--- util/bin/git.lua
+++
@@ -1,7 +0,0 @@
--- rudimentary GitHub client - may add support for other protocols, we shall see --
-
-local shell = require("shell")
-local internet = require("internet")
-local json = require("json")
-
-local args, opts = shell.parse(...)
