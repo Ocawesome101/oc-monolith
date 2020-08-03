3136fa1d419f1f3a3e16757f6be28d1d59eee217
Ocawesome101 <ocawesome101@gmail.com>
2020-08-03 17:08:04 -0400

    move scripts, update manpages accordingly

--- init/module/scripts.lua
+++ init/module/scripts.lua
@@ -1,11 +1,11 @@
-log("INFO", "Running scripts from /lib/scripts/...")
+log("INFO", "Running scripts from /lib/init/scripts/...")
 
-local files = kernel.filesystem.list("/lib/scripts/")
+local files = kernel.filesystem.list("/lib/init/scripts/")
 if files then
   table.sort(files)
   for k, v in ipairs(files) do
     log("WAIT", v)
-    local full = kernel.filesystem.concat("/lib/scripts/", v)
+    local full = kernel.filesystem.concat("/lib/init/scripts/", v)
     local ok, err = loadfile(full)
     if not ok then
       panic(err)
