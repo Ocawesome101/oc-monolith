dc4bb5ee1d8c1230435e30a4358c179448b120cd
Ocawesome101 <ocawesome101@gmail.com>
2020-08-23 21:03:54 -0400

    add more lines to grep

--- util/bin/grep.lua
+++ util/bin/grep.lua
@@ -12,3 +12,5 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details. ]]
 
 local shell = require("shell")
+
+local args, opts = shell.parse(...)
