a9d6a1d30a0ad44e3586588ce58d0b67205554a3
Ocawesome101 <ocawesome101@gmail.com>
2020-10-23 11:50:39 -0400

    cdoc is colored now, and less actually uses the term buffer

--- util/bin/less.lua
+++ util/bin/less.lua
@@ -30,7 +30,7 @@ local function drawLine(y, txt)
 end
 
 local function redraw(a)
-  io.write("\27[9m\27[2J")
+  io.write("\27(B\27[2J")
   local y = 1
   local n = 1
   while y < h - 1 do
@@ -42,7 +42,7 @@ local function redraw(a)
   else
     drawLine(h, "\27[2K"..a..":")
   end
-  io.write("\27[29m")
+  io.write("\27(b")
   --io.write(string.format("\27[%d;%dH%5d/%5d", w - 11, h, h + scroll, screen))
 end
 
